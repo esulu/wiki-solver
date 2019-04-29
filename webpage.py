36e69950d558ae127ecfb41d93fd17d6dc761455
Eren Sulutas <erensulu11@gmail.com>
2019-04-29 11:54:17 -0400

    added random page support

--- webpage.py
+++ webpage.py
@@ -1,15 +1,21 @@
-# Handles all things to do with Wikipedia web pages
+# handles all things to do with Wikipedia web pages
 
 import urllib.request
 from urllib import error
 import re
 
+# link to a random Wikipedia article
+random = 'https://en.wikipedia.org/wiki/Special:Random'
+
 
 def get_url(source):
 
     try:
-        url = urllib.request.urlopen(source)
-        return url
+        if source.lower() == 'r':
+            url = urllib.request.urlopen(random)
+        else:
+            url = urllib.request.urlopen(source)
+        return url.geturl()
 
     except urllib.error.URLError:
         return None
