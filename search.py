ed75fa72932bfabe6faca2a121cc9fa56a834a91
Eren Sulutas <erensulu11@gmail.com>
2019-04-30 15:09:25 -0400

    test code (functional)

--- search.py
+++ search.py
@@ -2,7 +2,6 @@
 # WIP
 
 import webpage
-from collections import deque
 from queue import *
 
 
@@ -10,17 +9,18 @@ def shortest_path(start_page, end_page):
     queue = Queue()             # queue of the current path
     shortest = {}               # set of shortest path
     visited = {}                # set of all previously visited pages
-    path = []                   # path of the current iteration
+    order = []                  # path of the current iteration
     length = 0                  # length of the current path
     min_length = float('inf')   # length of the shortest path
 
     visited[start_page] = None
 
     # start and end are the same
-    if webpage.page(start_page) == webpage.page(end_page):
-        return [webpage.page(start_page), webpage.page(end_page)], length
+    if start_page == end_page:
+        return [start_page, end_page], length
 
     # marks the sources node as visited and enqueues it
+    #queue.put(webpage.page(start_page))
     queue.put(start_page)
 
     while queue:
@@ -28,31 +28,43 @@ def shortest_path(start_page, end_page):
         # de-queue a vertex from the queue
         current = queue.get()
         length += 1
-        print("Current url: {}".format(webpage.page(current)))
+        print("Current url: " + current)
+        #print("Current url: {}".format(webpage.page(current)))
 
         # current length is greater than previous lowest length
         if length >= min_length:
-            return visited, length  # dip to next iteration?
+            print("\nspecial case")
+            return visited  # dip to next iteration?
 
         # iterate through all adjacent vertices (web pages) note: i is a link in string form
         for i in webpage.get_links(current):
-            print("Current adjacent: {}".format(webpage.page(webpage.get_url(i))))
+            print("Current adjacent: " + i)
             # end page is found to be adjacent to the current page
-            if i == webpage.page(end_page):
+            if i == end_page:
                 print("\nPAGE FOUND")
                 visited[i] = current
                 path = complete_path(visited, i)  # adds the current and the previous pages to the path
                 shortest = set_shortest(shortest, path)
-                return path, length  # remove the return statement? might be shorter one still
+                return path  # remove the return statement? might be shorter one still
 
             # adjacent vertex is already in the current shortest path
             elif i in shortest:
                 visited[i] = current
                 print("Found shortcut")
                 path = complete_path(visited, i)
-                path_rest = complete_path(shortest, i)
+                path_tail = complete_path(shortest, i)
+                path_tail.reverse()
+                path_tail.remove(i)
+                order = path + path_tail
+                min_length = len(order) - 1
 
-    return shortest, min_length
+            # add as a marked vertex if it is not already marked
+            else:
+                if i not in visited:
+                    visited[i] = current
+                    queue.put(i)
+
+    return shortest
 
 
 # makes a list of the entire path, iterating through the visited vertices
