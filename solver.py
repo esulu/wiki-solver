0a5d15f459cd2e3ea09d5e6acea1a1491e82b1a1
Eren Sulutas <erensulu11@gmail.com>
2019-05-01 09:37:01 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -7,23 +7,16 @@ print("Welcome to the Wikipedia solver! To begin, enter start/end pages, or type
 
 # user entry
 while True:
-    start_page = webpage.get_url('https://en.wikipedia.org/wiki/Pink_Floyd')#webpage.get_url(input("Starting wikipedia page: "))
-    end_page = webpage.get_url('https://en.wikipedia.org/wiki/Sheep')#webpage.get_url(input("Ending wikipedia page: "))
+    start_page = webpage.get_url(input("Starting wikipedia page: "))
+    end_page = webpage.get_url(input("Ending wikipedia page: "))
 
     if not start_page or not end_page:  # either start or end pages (or both) are invalid
         print("Invalid entry\n")
     else:
         break
 
-# TEST CODE BELOW
-
 print("\nEntry Complete\n")
 
-'''print(webpage.page(start_page))
-print(webpage.get_links(start_page))
-
-print("\n" + webpage.page(end_page))
-print(webpage.get_links(end_page))'''
 
 path = search.shortest_path(webpage.page(start_page), webpage.page(end_page))
 length = 0
@@ -32,4 +25,4 @@ for i in path:
     length += 1
 
 print(webpage.print_data(path))
-print("Length: " + str(length))
+print("Clicks Required: " + str(length-1))
